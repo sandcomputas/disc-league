276ffbdde2e14f05cb61e993761d1abe5e5847bb
Sondre Andersen <sand@computas.com>
2025-04-17 14:19:51 +0200

    moved navbar stuff to root component

--- disc-web/src/routes/__root.tsx
+++ disc-web/src/routes/__root.tsx
@@ -1,28 +1,43 @@
-import {createRootRoute, Outlet} from '@tanstack/react-router'
+import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/router-devtools'
 // @ts-ignore
 import styles from './root.module.css'
 // @ts-ignore
 import image from "@/assets/logo.png"
 import {RegisterNewRoundBtn} from "@/components/RegisterNewRoundBtn/RegisterNewRoundBtn.tsx";
+import { useState } from "react";
 
 
 export const Route = createRootRoute({
     component: RootComponent,
 })
-
+interface NavBarItem {
+  id: string;
+  name: string;
+  to: string;
+}
 function RootComponent() {
-
+  const tabs: NavBarItem[] = [
+    {id: 'leaderboard', name: 'Leaderboard', to: '/'},
+    {id: 'rounds', name: 'Rounds', to: '/rounds'}
+  ]
+  const [selectedTab, setSelectedTab] = useState(tabs[0].id)
     return (
-        <>
-            <div className={styles["logoContainer"]}>
-                <img className={styles["logo"]} src={image}/>
-            </div>
-            <div className={styles["outletContainer"]}>
-                <Outlet/>
-            </div>
-            <RegisterNewRoundBtn/>
-            <TanStackRouterDevtools position="bottom-right"/>
-        </>
+      <>
+        <div className={styles["logoContainer"]}>
+          <img className={styles["logo"]} src={image}/>
+        </div>
+        <div className={styles["outletContainer"]}>
+          <div className={styles.navbarContainer}>
+            {tabs.map((t) => (
+              <Link to="/" className={`${styles.navbarButton} ${selectedTab === t.id && styles.active}`}
+                    onClick={() => setSelectedTab(t.id)}>{t.name}</Link>
+            ))}
+          </div>
+          <Outlet/>
+        </div>
+        <RegisterNewRoundBtn/>
+        <TanStackRouterDevtools position="bottom-right"/>
+      </>
     )
 }
--- disc-web/src/routes/index.tsx
+++ disc-web/src/routes/index.tsx
@@ -1,34 +1,16 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { createFileRoute } from '@tanstack/react-router'
 // @ts-ignore
 import styles from './index.module.css';
 import LeaderboardTable from "@/components/LeaderboardTable/LeaderboardTable.tsx";
-import { useState } from "react";
 
 
 export const Route = createFileRoute('/')({
   component: LeaderBoardPage,
 })
-
-interface NavBarItem {
-  id: string;
-  name: string;
-  to: string;
-}
-
 function LeaderBoardPage() {
-  const tabs: NavBarItem[] = [
-    {id: 'leaderboard', name: 'Leaderboard', to: '/'},
-    {id: 'rounds', name: 'Rounds', to: '/rounds'}
-  ]
-  const [selectedTab, setSelectedTab] = useState(tabs[0].id)
 
   return (
-    <div className={styles.headerContainer}>
-      <div className={styles.navbarContainer}>
-        {tabs.map((t) => (
-          <Link to="/" className={`${styles.navbarButton} ${selectedTab === t.id && styles.active}`} onClick={() => setSelectedTab(t.id)}>{t.name}</Link>
-        ))}
-      </div>
+    <div>
       <h1>Leaderboard</h1>
       <LeaderboardTable/>
     </div>
